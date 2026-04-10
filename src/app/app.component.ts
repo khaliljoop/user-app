4ec4fd65db430e44613e6ee53c7199d7f3af4d2f
khaliljoop <info08.diop@gmail.com>
2026-04-10 10:43:58 +0000

    maj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UsersComponent } from "./pages/users/users.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UsersComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
